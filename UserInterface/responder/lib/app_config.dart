929e8aa86bc392ab8a3eb043041092616183877e
jeromoney <justin.matis.com@gmail.com>
2026-04-12 09:32:57 -0600

    a

--- UserInterface/responder/lib/app_config.dart
+++ UserInterface/responder/lib/app_config.dart
@@ -1,7 +1,5 @@
 const apiBaseUrl = String.fromEnvironment(
-  'API_BASE_URL',
-  defaultValue: 'http://127.0.0.1:8000',
-);
+  'API_BASE_URL');
 const webPushPublicKey = String.fromEnvironment(
   'WEB_PUSH_PUBLIC_KEY',
   defaultValue: '',
